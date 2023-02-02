efa06a71fc2a2016b90a3268e96cd83f7f59461d
surendra1993 <bharathi.goutham@gmail.com>
2023-02-02 15:17:56 -0700

    What is the sum of the priorities of those item types task completed

--- 2022/DAY 3/rucksack.js
+++ 2022/DAY 3/rucksack.js
@@ -1,40 +1,68 @@
-input_strings = [
-    "vJrwpWtwJgWrhcsFMMfFFhFp",
-    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
-    "PmmdzqPrVvPwwTWBwg",
-    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
-    "ttgJtRGJQctTZtZT",
-    "CrZsJsPPZsGzwwsLwLmpwMDw"
-]
+const fs = require('fs')
 
-same_item_type = [];
+fs.readFile('input.txt', 'utf-8', (err, data) => {
+    // If there was an error in reading input from file
+    if (err) throw err;
 
-for(string of input_strings){
-    // Since given rucksack always has the same number of items in each of its two compartments
-    first_compartment = string.slice(0, Math.floor(string.length / 2))
-    second_compartment = string.slice(Math.floor(string.length / 2), string.length);
+    input_strings = data.toString().split('\n');
 
-
-    console.log(first_compartment, "first_compartment")
-    console.log(second_compartment, "second_compartment")
-
-    set = [];
+    // console.log(input_strings);
     
-    validate_string(first_compartment, set, same_item_type, 1);
-    validate_string(second_compartment, set, same_item_type, 2);
+    // { 'p', 'L', 'P', 'v', 't', 's' } - Using set to avoid duplicate values
+    same_item_type = new Set();
+    sum = 0;
+    
+    for(string of input_strings){
+        // Since given rucksack always has the same number of items in each of its two compartments
+        first_compartment = string.slice(0, Math.floor(string.length / 2))
+        second_compartment = string.slice(Math.floor(string.length / 2), string.length);
+        // console.log(first_compartment, "first_compartment")
+        // console.log(second_compartment, "second_compartment")
+    
+        char_list = [];
+        
+        validate_string(first_compartment, char_list, same_item_type, 1);
+        validate_string(second_compartment, char_list, same_item_type, 2);
+    }
+    
+    // console.log(same_item_type)
+    
+    same_item_type.forEach(num => {
+        sum += num;
+      });
+    
+    //  Final returning value
+    // console.log(sum)  
+    return sum;
+    
+})
 
-    console.log(set);
-}
-
-console.log([...new Set(same_item_type)])
-
-function validate_string(string, set, item_type, counter){
+function validate_string(string, char_list, item_type, counter){
     for(char of string){
-        if(set.includes(char) && counter == 2){
-            item_type.push(char);
+        if(char_list.includes(char) && counter == 2){
+            priority = get_priority(char);
+            // console.log(priority);
+
+            item_type.add(priority);
         }
         else if (counter == 1){
-            set.push(char);
+            char_list.push(char);
         }
     }
+}
+
+function get_priority(char){
+    if(char == char.toUpperCase()){
+        // Char is Upper case. Eg: A
+        // A - Z = priority between 27 and 52. To cal priority for char. Subtracting 38 from ASCII code
+        ascii = char.charCodeAt(0);
+        priority = ascii - 38;
+    }
+    if(char == char.toLowerCase()){
+        // Char is lower case. Eg: a
+        // a - z = priority between 1 and 26. To cal priority for char. Subtracting 96 from ASCII code
+        ascii = char.charCodeAt(0);
+        priority = ascii - 96
+    }
+    return priority;
 }
